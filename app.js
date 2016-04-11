8a10e16e09c5a32e7b03e1dfc3155571a33c9b74
stern15 <ssemasuka@gmail.com>
2016-04-11 16:07:05 +0300

    adding more comment and now the input of the user has to have the keepword for it to work

--- app.js
+++ app.js
@@ -1,39 +1,55 @@
-var InputUser = prompt("what do you want to do?");
+//Todo array that will holds all the Todo
 var Todo = [];
-while (InputUser !== "quit") {
-    //displaying the input from the array
-    if (InputUser === "list") {
+//user selection at the beginning
+var UserIn = prompt("please enter what you want to do");
+//while the condition is not quit it will loop again and again
+while (UserIn.indexOf("quit") === -1) {
+    //if with the user input the is a "new" word,it will launch this if statement
+    if (UserIn.indexOf("new") !== -1) {
+        //calling this method
+        creatingTodo();
+    }
+    //if with the user input the is a "list" word,it will launch this else if statement
+    else if (UserIn.indexOf("list") !== -1) {
+        //calling this method
         listTodo();
+
     }
-    //adding a new Todo
-    else if (InputUser === "new") {
-        addTodo();
-    }
-    //delete in the Todo
-    else if (InputUser === "delete") {
+    //if with the user input the is a "delete" word,it will launch this else if statement
+    else if (UserIn.indexOf("delete") !== -1) {
+        //calling this method
         deleteTodo();
     }
-    //asking again for a new input
-    InputUser = prompt("what do you want to do?");
+    //after executing one of the statement above,it will prompt again this
+    var UserIn = prompt("please enter what you want to do");
 }
-console.log("ok,you have quit the app");
+//display this after quitting the app
+console.log("Ok,you have existed the app");
+
+function creatingTodo() {
+    var newTodo = prompt("please enter your new Todo");
+    //adding to the array Todo from what the users have added in the prompt
+    Todo.push(newTodo);
+    //inform that the Todo have been successfully added
+    console.log("Successfully added the Todo");
+}
 
 function listTodo() {
-    console.log("* * * * * * * * * * * * * * * * * * * * * * * * * *");
+    //decoration
+    console.log("*****************************");
+    //loop through the array using forEach and adding the second argument(as index which is always a number) and assign it the Todo
     Todo.forEach(function (each, i) {
-        console.log(i + " : " + each);
-    });
-    console.log("* * * * * * * * * * * * * * * * * * * * * * * * * *");
+            console.log(i + " : " + each);
+        })
+        //decoration
+    console.log("*****************************");
 }
 
-function addTodo() {
-    var InputTodo = prompt("Please enter your Todo");
-    Todo.push(InputTodo);
-    console.log("Successfully added Todo");
-}
 
 function deleteTodo() {
-    var deleteIndex = prompt("please enter the index of the element to be deleted");
-    Todo.splice(deleteIndex, 1);
-    console.log("Successfully deleted Todo");
+    var selectIn = prompt("please enter an index of the Todo to delete");
+    //to delete the Todo in the array,the second argument is how many of the index have to be deleted
+    Todo.splice(selectIn, 1);
+    //a message that tells that the index has been deleted
+    console.log("successfully deleted the selected Todo");
 }
